91969979996a0149ccb64887094398cec1c7b782
WilliamChampagne <William_Champagne@hotmail.com>
2025-05-14 10:25:21 -0400

    Ajout de la route doc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import fs from 'fs';
 import cors from 'cors';
-
+import swaggerUi from 'swagger-ui-express';
 import tachesRoutes from './src/routes/taches.route.js';
 
 // Créer une application express
@@ -11,6 +11,12 @@ app.use(cors());
 
 // Importer les middlewares
 app.use(express.json());
+const swaggerDocument = JSON.parse(fs.readFileSync('./src/config/documentation.json', 'utf8'));
+
+const swaggerOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "Projet Final SW25 - William Champagne"
+};
 
 //Configuration de morgan
 const accessLogStream = fs.createWriteStream('./src/logs/access.log', {flags: 'a' })
@@ -21,6 +27,8 @@ app.use(morgan('combined', { stream: accessLogStream}));
 // ===============
 
 app.use('/api/taches', tachesRoutes);
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 
 // Démarrer le serveur
